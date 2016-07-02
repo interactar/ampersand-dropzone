9912cc891521175e6cf4f24218174af719cb870e
Facundo Gonzalez <facugon@interactar.com>
2016-07-02 19:09:57 -0300

    add more features

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var View = require('ampersand-view');
 var Dropzone = require('dropzone');
 Dropzone.autoDiscover = false;
+var debug = require('debug')('dropzone');
 
 module.exports = View.extend({
   props:{
     maxFiles:['number',false,1],
     maxFilesize:['number',false,5],
-    url:['string',false,'/dropzone/upload'],
     parallelUploads:['number',false,1],
     uploadMultiple:['boolean',false,false],
     addRemoveLinks:['boolean',false,true],
     autoProcessQueue:['boolean',false,true],
     method:['string',false,'POST'],
     hideSaveButton:['boolean',false,false],
-    dzEvents:['object',false,function(){
-      return {};
-    }]
+    fileTypes:['array',false,function(){ return[]; }],
+    url:['any',false,function(){
+      debug('url not set yet. remember to use a funtion!');
+      return '';
+    }],
+    dzEvents:['object',false,function(){ return {}; }],
   },
   derived:{
     enableSaveButton:{
@@ -23,7 +26,13 @@ module.exports = View.extend({
       fn:function(){
         return ! this.autoProcessQueue && ! this.hideSaveButton;
       }
-    }
+    },
+    acceptedFiles:{
+      deps:['fileTypes'],
+      fn:function(){
+        return fileTypes.join(',');
+      }
+    },
   },
   bindings:{
     enableSaveButton:{
@@ -38,40 +47,47 @@ module.exports = View.extend({
   onClickSaveButton:function(event){
     event.preventDefault();
     event.stopPropagation();
-    if(this.dropzone.files.length>0){
+    if(this.dropzone && this.dropzone.files.length>0){
       this.dropzone.processQueue();
     } else {
+      debug('no file selected. button should be disabled');
     }
   },
   render:function(){
+    var self = this;
     this.renderWithTemplate(this);
 
-    var container = this.queryByHook('dropzone');
     // when Dropzone is instantiated dz autorender itself into the container element
-    this.dropzone = new Dropzone(container,this._values);
+    var container = this.queryByHook('dropzone');
+    var dropzone = new Dropzone(container,this._values);
+    this.dropzone = dropzone;
 
-    var self = this;
-    for(var eventName in this.dzEvents){
-      var dzEventHandler = this.dzEvents[eventName];
+    this.listenTo(this,'change',this.updateDropzone);
+    this.setDzEvents(this.dzEvents);
+  },
+  updateDropzone:function(){
+    this.setDzEvents(this.dzEvents);
+  },
+  setDzEvents:function(dzEvents){
+    for(var eventName in dzEvents){
       this.dropzone.on(eventName,function(){
-        dzEventHandler.apply(self,arguments);
+        console.log('handling dropzone event ', eventName);
+        dzEvents[eventName](arguments);
       });
     }
   },
-  save:function(){
-    if(this.dropzone.files.length==0)
-      if(this.dzEvents.complete)
-        this.dzEvents.complete.call(self,{});
+  save:function(options){
+    options||(options={});
+    if(options.dzEvents){
+      this.setDzEvents(options.dzEvents);
+    }
 
-    else this.dropzone.processQueue();
-  },
-  onSuccess:function(){
-    console.log('dropzone success event ',arguments);
-  },
-  onError:function(){
-    console.log('dropzone error event ',arguments);
-  },
-  onComplete:function(){
-    console.log('dropzone complete event ',arguments);
+    if(this.dropzone.files.length==0){
+      if(this.dzEvents.complete){
+        this.dzEvents.complete({});
+      }
+    } else {
+      this.dropzone.processQueue();
+    }
   }
 });
